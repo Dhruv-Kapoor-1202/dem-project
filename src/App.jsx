b062075eaa6528d96a0b66593d2cf3edf6e7fe46
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-10-27 15:31:23 +0530

    fix: add animations

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,14 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-start gap-4 mb-10">
+      <div className="flex flex-col items-center justify-start gap-4 mb-10 animate-fade-in">
         <Navbar onSearch={handleSearch} />
-        <div className="w-full max-w-screen-md border divide-y-2 sm:rounded-xl divide-white/5 bg-gray-950/90 backdrop-blur-sm border-white/15">
+        <div className="w-full max-w-screen-md border divide-y-2 animate-fade-in sm:rounded-xl divide-white/5 bg-gray-950/90 backdrop-blur-sm border-white/15">
+          <div className="grid grid-cols-4 p-4 text-gray-400 duration-200 ">
+            <h3 className="col-span-2">Name</h3>
+            <p className="col-span-1">Location</p>
+            <p className="text-right">Date</p>
+          </div>
           {filteredEvents.length > 0 ? (
             filteredEvents.map((event) => (
               <EventCard
--- src/components/EventModal.jsx
+++ src/components/EventModal.jsx
@@ -6,23 +6,18 @@ const EventModal = ({ event, onClose }) => {
 
   return (
     <div
-      className="fixed flex flex-col items-center justify-center w-full h-screen p-4 bg-black/5 backdrop-blur-sm"
+      className="fixed flex flex-col items-center justify-center w-full h-screen p-4 bg-black/5 backdrop-blur-sm "
       onClick={onClose}
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="flex flex-col w-full max-w-screen-md overflow-hidden transition border shadow-sm bg-gray-950 group rounded-xl focus:outline-none focus:shadow-lg border-white/15"
+        className="flex flex-col w-full max-w-screen-md border shadow-sm overflow-clip animate-scale-up bg-gray-950 group rounded-xl focus:outline-none focus:shadow-lg border-white/15 "
       >
         <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
-          {/* <img
-            className="absolute top-0 object-cover transition-transform duration-500 ease-in-out size-full start-0 group-hover:scale-105 group-focus:scale-105 rounded-t-xl"
-            src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80"
-            alt="Card Image"
-          /> */}
           <button
             type="button"
             onClick={onClose}
-            className="absolute z-10 inline-flex items-center justify-center p-2 text-gray-400 duration-200 rounded-md backdrop-blur-sm bg-white/60 top-5 right-5 w-fit hover:text-gray-500 hover:bg-white/90 hover:backdrop-blur-lg focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            className="absolute z-10 inline-flex items-center justify-center p-2 text-gray-700 duration-200 rounded-md backdrop-blur-sm bg-white/60 top-5 right-5 w-fit hover:text-gray-500 hover:bg-white/90 hover:backdrop-blur-lg focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
           >
             <span className="sr-only">Close menu</span>
             <svg
@@ -42,15 +37,16 @@ const EventModal = ({ event, onClose }) => {
             </svg>
           </button>
           <img
-            className="absolute top-0 object-cover transition-transform duration-500 ease-in-out size-full start-0 group-hover:scale-105 group-focus:scale-105 rounded-t-xl bg-gradient-to-tr from-gray-200 to-gray-300"
+            className="absolute top-0 left-0 object-cover w-full h-full transition-transform duration-500 ease-in-out group-hover:scale-105 group-focus:scale-105 rounded-t-xl bg-gradient-to-tr from-gray-200 to-gray-300"
+            loading="lazy"
             src={event.image}
             alt={event.name}
           />
         </div>
         <div className="p-4 md:p-5">
           <h3 className="text-lg font-bold text-white">
-            {event.name}{" "}
-            <span className="text-xs font-bold text-neutral-400">
+            {event.id}. {event.name}{" "}
+            <span className="text-xs font-semibold text-neutral-400">
               {event.date}
             </span>
           </h3>
